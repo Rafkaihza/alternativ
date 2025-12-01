8d9c38e6a21d5e0322a7a6e60ca352da5379acc9
Muhammad Rafkaihza Hardandi <145969975+Rafkaihza@users.noreply.github.com>
2025-12-01 17:30:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World from CI/CD to Kubernetes rafka! 🚀");
+  res.send("Hello World from CI/CD to Kubernetes! 🚀");
 });
 
 app.listen(port, () => {
