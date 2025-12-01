face909d537236ab01c0e72323e8de4b36381d33
Rafkaihza <rafkaihzah@gmail.com>
2025-12-01 17:47:32 +0700

    push

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World from CI/CD to Kubernetes! 🚀");
+  res.send("Hello World from CI/CD to Kubernetes rafkaihza! 🚀");
 });
 
 app.listen(port, () => {
