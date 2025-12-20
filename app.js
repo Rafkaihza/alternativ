47158ed7f4904acf5e98802930c38c0a7eeeff95
Rafkaihza <rafkaihzah@gmail.com>
2025-12-20 20:31:20 +0700

    p

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World from CI/CD update🚀");
+  res.send("Hello World from CI/CD🚀");
 });
 
 app.listen(port, () => {
